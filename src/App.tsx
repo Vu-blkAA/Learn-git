0fb3f19e5b3d33664cfe4367d6985482a6c505e4
Vu Luong <vul@Liberty-MacBook-Pro-M1.local>
2025-01-20 15:30:54 +0700

    build/1: 1.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -7,13 +5,7 @@ function App() {
   return (
     <>
       <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-        Merge commit 1.5
+        Merge commit 1.6
       </div>
       <h1>Vite + React</h1>
     </>
